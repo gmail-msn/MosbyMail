bc6d88fb7de46c9588f5675f39bb7559e19200df
Haisen <haisen@yahoo.com>
2015-09-24 15:17:44 +0800

    initialize mosby mail sample repository

---
+++ app/src/main/java/com/hannesdorfmann/mosby/sample/mail/label/LabelLayoutComponent.java
@@ -0,0 +1,22 @@
+package com.hannesdorfmann.mosby.sample.mail.label;
+
+import com.hannesdorfmann.mosby.sample.mail.dagger.MailAppComponent;
+import com.hannesdorfmann.mosby.sample.mail.dagger.MailModule;
+
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+/**
+ * @author Hannes Dorfmann
+ */
+
+@Singleton
+@Component(
+        modules = MailModule.class,
+        dependencies = MailAppComponent.class
+)
+public interface LabelLayoutComponent {
+
+    LabelPresenter presenter();
+}
---
+++ app/src/main/java/com/hannesdorfmann/mosby/sample/mail/model/event/MailUnstaredEvent.java
@@ -0,0 +1,18 @@
+package com.hannesdorfmann.mosby.sample.mail.model.event;
+
+/**
+ * Fired to inform that an Mail has been starred
+ *
+ * @author Hannes Dorfmann
+ */
+public class MailUnstaredEvent {
+    private int mailId;
+
+    public MailUnstaredEvent(int mailId) {
+        this.mailId = mailId;
+    }
+
+    public int getMailId() {
+        return mailId;
+    }
+}
